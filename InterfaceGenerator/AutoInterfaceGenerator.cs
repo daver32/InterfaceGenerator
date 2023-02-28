74337459d3a87b054a728c6cff43e50823f3acf1
Adam Hathcock <adam@hathcock.uk>
2023-02-28 10:48:31 +0000

    test annotation

--- InterfaceGenerator/AutoInterfaceGenerator.cs
+++ InterfaceGenerator/AutoInterfaceGenerator.cs
@@ -146,7 +146,10 @@ namespace InterfaceGenerator
             var visibilityModifier = InferVisibilityModifier(implTypeSymbol, attributeData);
 
             //https://stackoverflow.com/questions/55492214/the-annotation-for-nullable-reference-types-should-only-be-used-in-code-within-a fix for nullable
-            codeWriter.WriteLine("#nullable enable");
+            if (implTypeSymbol.NullableAnnotation == NullableAnnotation.Annotated)
+            {
+                codeWriter.WriteLine("#nullable enable");
+            }
             codeWriter.WriteLine("namespace {0}", namespaceName);
             codeWriter.WriteLine("{");
 
@@ -165,7 +168,10 @@ namespace InterfaceGenerator
             --codeWriter.Indent;
 
             codeWriter.WriteLine("}");
-            codeWriter.WriteLine("#nullable restore");
+            if (implTypeSymbol.NullableAnnotation == NullableAnnotation.Annotated)
+            {
+                codeWriter.WriteLine("#nullable restore");
+            }
 
             codeWriter.Flush();
             stream.Seek(0, SeekOrigin.Begin);
